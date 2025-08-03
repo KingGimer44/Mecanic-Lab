dfad057d05e0772e2dbc3f93e8e1d4339f34551d
KingGimer44 <150565033+KingGimer44@users.noreply.github.com>
2025-08-03 00:28:37 -0500

    Update index.js

--- api-empleados/api/index.js
+++ api-empleados/api/index.js
@@ -227,7 +227,7 @@ module.exports = async (req, res) => {
         const { id, name, is_available } = JSON.parse(body);
         await db.execute({
           sql: `INSERT INTO parts (id, name, is_available) VALUES (?, ?, ?)` ,
-          args: [id, name, is_available || true]
+          args: [id, name, is_available || 0]
         });
         res.status(201).json({ message: "Pieza creada" });
       } catch (err) {
